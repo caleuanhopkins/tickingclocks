e00264077d77fce7a79285547f1d377363991daa
Callum Hopkins <callum.e.hopkins@gmail.com>
2015-12-08 15:50:56 +0000

    added in seconds indicator and numbers

--- js/mods/jquery.clock.js
+++ js/mods/jquery.clock.js
@@ -24,11 +24,14 @@
 			clockHourHandWidth: 0,
 			clockHandLength: 6,
 			clockPinColor: "#444444",
-			clockScaleRatio: 2,
 			clockPinSize: 0,
+			showTimeNumbers: true,
 			showTimeStrokes: true,
 			clockTimeStrokeColor: "#444444",
 			clockTimeStrokeWidth: 2,
+			clockMiniTimeStrokeColor: "#949494",
+			clockMiniTimeStrokeWidth: 1,
+			showMiniSecs: false,
             onEachSecond: function(now, element, hours, minutes, seconds, settings){
             	console.log(now)
             }
@@ -76,13 +79,37 @@
 		  this.parts.clock.attr({"fill":settings.clockBGColor,"stroke":settings.clockBorderColor,"stroke-width":settings.clockBorderWidth})
 		  if(settings.showTimeStrokes){
 		  	this.parts.hour_sign = []
-			  for(var i=0;i<12;i++){
-			    var start_x = (settings.clockSize/2)+Math.round((((settings.clockSize/2)/2)+(settings.clockSize/5))*Math.cos(30*i*Math.PI/180));
-			    var start_y = (settings.clockSize/2)+Math.round((((settings.clockSize/2)/2)+(settings.clockSize/5))*Math.sin(30*i*Math.PI/180));
-			    var end_x = (settings.clockSize/2)+Math.round(((settings.clockSize/2)-(settings.clockSize/12))*Math.cos(30*i*Math.PI/180));
-			    var end_y = (settings.clockSize/2)+Math.round(((settings.clockSize/2)-(settings.clockSize/12))*Math.sin(30*i*Math.PI/180));  
-			    this.parts.hour_sign[i] = canvas.path("M"+start_x+" "+start_y+"L"+end_x+" "+end_y);
-			    this.parts.hour_sign[i].attr({"stroke": settings.clockTimeStrokeColor, "stroke-width": settings.clockTimeStrokeWidth})
+		  	  if(!settings.showTimeNumber){
+				  for(var i=0;i<12;i++){
+				    var start_x = (settings.clockSize/2)+Math.round((((settings.clockSize/2)/2)+(settings.clockSize/5))*Math.cos(30*i*Math.PI/180));
+				    var start_y = (settings.clockSize/2)+Math.round((((settings.clockSize/2)/2)+(settings.clockSize/5))*Math.sin(30*i*Math.PI/180));
+				    var end_x = (settings.clockSize/2)+Math.round(((settings.clockSize/2)-(settings.clockSize/12))*Math.cos(30*i*Math.PI/180));
+				    var end_y = (settings.clockSize/2)+Math.round(((settings.clockSize/2)-(settings.clockSize/12))*Math.sin(30*i*Math.PI/180));
+			    	this.parts.hour_sign[i] = canvas.path("M"+start_x+" "+start_y+"L"+end_x+" "+end_y);
+			    	this.parts.hour_sign[i].attr({"stroke": settings.clockTimeStrokeColor, "stroke-width": settings.clockTimeStrokeWidth})
+				  }
+			  }else{
+			  	  for(var i=0;i<13;i++){
+			  		var num = i-3
+				    var start_x = (settings.clockSize/2)+Math.round((((settings.clockSize/2)/2)+(settings.clockSize/6))*Math.cos(30*num*Math.PI/180));
+				    var start_y = (settings.clockSize/2)+Math.round((((settings.clockSize/2)/2)+(settings.clockSize/6))*Math.sin(30*num*Math.PI/180));
+			  		this.parts.hour_sign[i] = canvas.text(start_x, start_y, i)
+					this.parts.hour_sign[i].attr({"fill":settings.clockTimeStrokeColor, "stroke": settings.clockTimeStrokeColor})				  	
+					var num = i-3;
+				  }
+			  }
+			  if(settings.showMiniSecs){
+			  	this.parts.min_sign = []
+			  	for(var i=0;i<60;i++){		
+			  		if(((12*i)/5) % 1 != 0){			    
+			  			var start_x = (settings.clockSize/2)+Math.round((((settings.clockSize/2)/2)+(settings.clockSize/5))*Math.cos(12*i*Math.PI/360));
+					    var start_y = (settings.clockSize/2)+Math.round((((settings.clockSize/2)/2)+(settings.clockSize/5))*Math.sin(12*i*Math.PI/360));
+					    var end_x = (settings.clockSize/2)+Math.round(((settings.clockSize/2)-(settings.clockSize/14))*Math.cos(12*i*Math.PI/360));
+					    var end_y = (settings.clockSize/2)+Math.round(((settings.clockSize/2)-(settings.clockSize/14))*Math.sin(12*i*Math.PI/360));
+				    	this.parts.min_sign[i] = canvas.path("M"+start_x+" "+start_y+"L"+end_x+" "+end_y);
+				    	this.parts.min_sign[i].attr({"stroke": settings.clockMiniTimeStrokeColor, "stroke-width": settings.clockMiniTimeStrokeWidth})
+				    }
+			    }		  	
 			  }
 		  }
 		  this.parts.hour_hand = canvas.path("M"+(settings.clockSize/2)+" "+(settings.clockSize/2)+"L"+(settings.clockSize/2)+" "+(((settings.clockSize/settings.clockHandLength)/0.4)));
@@ -99,17 +126,15 @@
 
 		draw_clock: function (timezone, elm){
 		  var now = new Date();
-		  if (now.dst()) { timezone-- }
 		  var hours = now.getUTCHours() + timezone;
-		  var armyHours = now.getHours() + timezone;
 		  var minutes = now.getUTCMinutes();
 		  if(elm.settings.showSeconds){
 		  	var seconds = now.getUTCSeconds();
 		  }
-		  this.parts.hour_hand.rotate(30*hours+(minutes/2.5), (elm.settings.clockSize/elm.settings.clockScaleRatio), (elm.settings.clockSize/elm.settings.clockScaleRatio));
-		  this.parts.minute_hand.rotate(6*minutes, (elm.settings.clockSize/elm.settings.clockScaleRatio), (elm.settings.clockSize/elm.settings.clockScaleRatio));
+		  this.parts.hour_hand.rotate(30*hours+(minutes/2.5), (elm.settings.clockSize/2), (elm.settings.clockSize/2));
+		  this.parts.minute_hand.rotate(6*minutes, (elm.settings.clockSize/2), (elm.settings.clockSize/2));
 		  if(elm.settings.showSeconds){
-		  	this.parts.second_hand.rotate(6*seconds, (elm.settings.clockSize/elm.settings.clockScaleRatio), (elm.settings.clockSize/elm.settings.clockScaleRatio));
+		  	this.parts.second_hand.rotate(6*seconds, (elm.settings.clockSize/2), (elm.settings.clockSize/2));
 		  }
 		  elm.settings.onEachSecond(now, elm, hours, minutes, seconds, this)
 		}
@@ -125,13 +150,3 @@
 	};
 
 })( jQuery, window, document );
-
-Date.prototype.stdTimezoneOffset = function() {
-    var jan = new Date(this.getFullYear(), 0, 1);
-    var jul = new Date(this.getFullYear(), 6, 1);
-    return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
-}
-
-Date.prototype.dst = function() {
-    return this.getTimezoneOffset() < this.stdTimezoneOffset();
-}
